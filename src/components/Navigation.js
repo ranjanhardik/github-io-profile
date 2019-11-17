cfc03af76928152356c05113392e0b87b0d23d37
Asheesh Kumar <asheesh.kumar3496@gmail.com>
2019-11-17 23:31:03 +0530

    Hamburger style (#10)

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -30,7 +30,7 @@ class Navigation extends Component {
   render() {
     return (
       <nav
-        className={`navbar navbar-expand-lg p-3 navigation ${
+        className={`navbar navbar-expand-lg navbar-light p-3 navigation ${
           this.state.scrolled ? "scrolled" : "not-scrolled"
         }`}
       >
@@ -41,15 +41,19 @@ class Navigation extends Component {
           className="navbar-toggler"
           type="button"
           data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
+          data-target="#navbarAsheesh"
+          aria-controls="navbarAsheesh"
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span className="navbar-toggler-icon"></span>
+          <div className="toggle-icon">
+            <div className="toggle-icon-one"></div>
+            <div className="toggle-icon-two"></div>
+            <div className="toggle-icon-three"></div>
+          </div>
         </button>
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <div className="collapse navbar-collapse" id="navbarAsheesh">
           <ul className="navbar-nav ml-auto">
             <li className="nav-item active px-3">
               <a className="nav-link" href="#">
